490ef1d482353e538f316a25bfbdaf15cd5cb2d0
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2023-09-26 13:24:26 +0300

    working on it

--- app/src/main/java/com/cosmah/basiccalculator/MainActivity.java
+++ app/src/main/java/com/cosmah/basiccalculator/MainActivity.java
@@ -1,13 +1,43 @@
 package com.cosmah.basiccalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText txtEntry = (EditText) findViewById(R.id.txtEntry);
+        txtEntry.setText("Hello from Java");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            txtEntry.setShowSoftInputOnFocus(false);
+        } else {
+            txtEntry.setTextIsSelectable(true);
+        }
+
+
+        TextView txtView = (TextView) findViewById(R.id.txtView);
+        txtView.setText("I love coding");
+
+
+        Button myButton = (Button) findViewById(R.id.btnseven);
+        myButton.setOnClickListener(v -> {
+            String currentText = txtEntry.getText().toString();
+            currentText += "7";
+            txtEntry.setText(currentText);
+        });
+
+
     }
 }
